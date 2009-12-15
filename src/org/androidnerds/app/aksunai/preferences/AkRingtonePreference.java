3e93964138e6a07353ae1fde89fa0fbc2aa27ad9
Mike Novak <mike@androidnerds.org>
2009-12-15 10:16:18 -0500

    added the ringtone option to the preferences.

--- src/org/androidnerds/app/aksunai/preferences/AkRingtonePreference.java
+++ src/org/androidnerds/app/aksunai/preferences/AkRingtonePreference.java
@@ -17,9 +17,13 @@
  */
 package org.androidnerds.app.aksunai.preferences;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.preference.RingtonePreference;
 import android.util.AttributeSet;
+import android.util.Log;
 
 public class AkRingtonePreference extends RingtonePreference {
 
@@ -27,5 +31,23 @@ public class AkRingtonePreference extends RingtonePreference {
 	
 	public AkRingtonePreference(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		
+		Intent intent = ((Activity) context).getIntent();
+		mProviderId = intent.getLongExtra("providerId", -1);
+		
+		if (mProviderId < 0) {
+			Log.e("Aksunai", "RingtonePreference needs a provider id.");
+		}
+	}
+	
+	protected Uri onRestoreRingtone() {
+		String ringtone = this.getSharedPreferences().getString("notification-ringtone", "");
+		Uri result = Uri.parse(ringtone);
+		
+		return result;
+	}
+	
+	protected void onSaveRingtone(Uri ringtone) {
+		getEditor().putString("notification-ringtone", ringtone.toString()).commit();
 	}
 }
