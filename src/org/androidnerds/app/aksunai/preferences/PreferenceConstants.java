1f35cffb294cb8712a7f16e0a15abf0d2438a493
Mike Novak <mike@androidnerds.org>
2009-12-16 16:12:16 -0500

    adding in the java constants for preference keys.

---
+++ src/org/androidnerds/app/aksunai/preferences/PreferenceConstants.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2009  AndroidNerds.org
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.androidnerds.app.aksunai.preferences;
+
+/**
+ * 
+ * @author mike@androidnerds.org
+ *
+ */
+public class PreferenceConstants {
+	public static final String LARGE_FONT = "pref_large_font_key";
+	public static final String NOTIFICATIONS = "pref_notification_bar";
+	public static final String RINGTONE = "notification-ringtone";
+	public static final String VIBRATE = "pref_notification_vibrate";
+	public static final String LOGGING = "pref_logging_key";
+	public static final String LOG_ONLY_NICK = "pref_log_only_nick";
+}
