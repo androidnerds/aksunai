b0c06a77f3eed8d9fdec57edb5ccee5c7de3a6ae
Mathieu Agopian <mathieu@trustive.com>
2009-12-17 19:25:06 +0100

    Created an empty .net.ConnectionManager

---
+++ src/org/androidnerds/app/aksunai/net/ConnectionManager.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2009  AndroidNerds.org
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.androidnerds.app.aksunai.net;
+
+import android.util.Log;
+import java.util.Collections;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.androidnerds.app.aksunai.irc.Server;
+import org.androidnerds.app.aksunai.util.AppConstants;
+
+/**
+ * ConnectionManager handles the network connections to the servers. It also holds a list of
+ * {@link org.androidnerds.app.aksunai.irc.Server} to which the messages from the servers will
+ * be sent, and from which the messages to the servers will be received.
+ */
+public class ConnectionManager {
+    private Map<String, Server> mServers;
+
+    /**
+     * takes a formatted string and writes it to the connection to the server wich corresponds to the
+     * {@link org.androidnerds.app.aksunai.irc.Server}
+     */
+    public void sendMessage(Server server, String message) {
+    }
+}
+
